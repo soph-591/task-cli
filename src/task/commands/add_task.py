7340b21ca63ba6d15509ce6bd7e86d536a5bb8f7
soph-591 <sophiepaul591@gmail.com>
2025-12-06 09:10:34 +0000

    bug

--- src/task/commands/add_task.py
+++ src/task/commands/add_task.py
@@ -5,7 +5,7 @@ def run(args):
     data = db.read_db("db.json")
     task_id = db.next_id(data)
     data[task_id] = task.new_task(args.description)
-    print(f"Task added successfully (ID: {task_id})"
+    print(f"Task added successfully (ID: {task_id})")
     db.write_db(data, "db.json")
 
 def register(subparsers):
